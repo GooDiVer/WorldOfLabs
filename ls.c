71b9c1416cde354b36adc046d64e9a756cd3d41a
GooDiVer <31765143+GooDiVer@users.noreply.github.com>
2017-12-25 12:52:18 +0300

    ls edited

--- ls.c
+++ ls.c
@@ -3,16 +3,24 @@
 #include <dirent.h>
 
 int main(int argc,char** argv) {
-	DIR* ptr = opendir(argv[1]);
-	struct dirent* file;
 	
-	if(ptr != NULL) 
-	{
-		while((file = readdir(ptr)) != NULL)
-			printf("%s\n",file->d_name);
-	}
-	else 
-		printf("Dir error occured.");
+	struct dirent* file;
+	char* papka;
+	if(argc == 1)
+		papka = ".";
+	else
+		papka = argv[1];
+	
+	DIR* ptr = opendir(papka);
+		
+		if(ptr != NULL) 
+		{
+			while((file = readdir(ptr)) != NULL)
+				printf("%s\n",file->d_name);
+		}
+		else 
+			printf("Dir error occured.");
+	
 	closedir(ptr);
 	return 0;
 }
